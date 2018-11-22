20e1429f87b7a8bfa06e68ad10160307d1d528f0
Akmal Bashan <akmal.squal@gmail.com>
2018-11-22 18:33:58 +0700

    update ignore file

---
+++ application/logs/log-2018-11-22.php
@@ -0,0 +1,44 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-11-22 08:33:16 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ')' at line 3 - Invalid query: SELECT *
+FROM `course`
+WHERE `id` IN()
+ERROR - 2018-11-22 08:33:17 --> Severity: error --> Exception: Call to a member function result_array() on boolean /home/vagrant/aprdi-e-learning/application/views/frontend/default/my_wishlist.php 35
+ERROR - 2018-11-22 08:34:06 --> Severity: Warning --> include(/navigation.php): failed to open stream: No such file or directory /home/vagrant/aprdi-e-learning/application/views/backend/index.php 24
+ERROR - 2018-11-22 08:34:06 --> Severity: Warning --> include(): Failed opening '/navigation.php' for inclusion (include_path='.:/usr/share/php') /home/vagrant/aprdi-e-learning/application/views/backend/index.php 24
+ERROR - 2018-11-22 08:34:06 --> Severity: Warning --> include(/courses.php): failed to open stream: No such file or directory /home/vagrant/aprdi-e-learning/application/views/backend/index.php 28
+ERROR - 2018-11-22 08:34:06 --> Severity: Warning --> include(): Failed opening '/courses.php' for inclusion (include_path='.:/usr/share/php') /home/vagrant/aprdi-e-learning/application/views/backend/index.php 28
+ERROR - 2018-11-22 08:34:12 --> Severity: Warning --> include(/navigation.php): failed to open stream: No such file or directory /home/vagrant/aprdi-e-learning/application/views/backend/index.php 24
+ERROR - 2018-11-22 08:34:12 --> Severity: Warning --> include(): Failed opening '/navigation.php' for inclusion (include_path='.:/usr/share/php') /home/vagrant/aprdi-e-learning/application/views/backend/index.php 24
+ERROR - 2018-11-22 08:34:12 --> Severity: Warning --> include(/courses.php): failed to open stream: No such file or directory /home/vagrant/aprdi-e-learning/application/views/backend/index.php 28
+ERROR - 2018-11-22 08:34:12 --> Severity: Warning --> include(): Failed opening '/courses.php' for inclusion (include_path='.:/usr/share/php') /home/vagrant/aprdi-e-learning/application/views/backend/index.php 28
+ERROR - 2018-11-22 09:43:55 --> 404 Page Not Found: Admin/backends
+ERROR - 2018-11-22 09:44:05 --> 404 Page Not Found: Admin/backend
+ERROR - 2018-11-22 09:44:13 --> 404 Page Not Found: Admin/backend
+ERROR - 2018-11-22 09:46:10 --> 404 Page Not Found: backend/Quizmanagement/quizmanagement
+ERROR - 2018-11-22 09:48:22 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 09:48:48 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 09:49:50 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 09:49:59 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/QuizManagement.php 8
+ERROR - 2018-11-22 09:50:06 --> 404 Page Not Found: backend//index
+ERROR - 2018-11-22 09:51:39 --> 404 Page Not Found: backend//index
+ERROR - 2018-11-22 09:51:57 --> 404 Page Not Found: backend/$1/index
+ERROR - 2018-11-22 09:54:21 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:03:34 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:03:41 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:04:38 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:05:32 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:05:34 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:06:55 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:06:59 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:08:35 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:10:06 --> Severity: error --> Exception: Call to a member function userdata() on null /home/vagrant/aprdi-e-learning/application/controllers/backend/Quizmanagement.php 8
+ERROR - 2018-11-22 10:44:46 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:44:46 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:44:46 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:44:55 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:44:55 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:44:56 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:45:27 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:45:27 --> 404 Page Not Found: Assets/frontend
+ERROR - 2018-11-22 10:45:28 --> 404 Page Not Found: Assets/frontend
