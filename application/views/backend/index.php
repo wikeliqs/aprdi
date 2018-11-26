f37540d7330b276dd259c762012c1605b645266d
Akmal Bashan <akmal.squal@gmail.com>
2018-11-26 14:58:35 +0700

    Update from kompi kantor

--- application/views/backend/index.php
+++ application/views/backend/index.php
@@ -16,20 +16,20 @@ $logged_in_user_role = strtolower($this->session->userdata('role'));
     <link name="favicon" type="image/x-icon" href="<?php echo base_url() . 'assets/favicon.png' ?>"
           rel="shortcut icon"/>
     <!-- BEGIN PLUGIN CSS -->
-	<?php include 'layout/includes_top.blade.php'; ?>
+	<?php include 'includes_top.php'; ?>
 </head>
 <!-- END HEAD -->
 <body class="page-body" data-url="http://neon.dev">
 <!-- BEGIN CONTAINER -->
 <div class="page-container">
-	<?php include 'layout/navigation.blade.php' ?>
+	<?php include 'navigation.php' ?>
     <div class="main-content">
-		<?php include 'layout/header.blade.php'; ?>
+		<?php include 'header.php'; ?>
 		
 		<?php include $logged_in_user_role . '/' . $page_name . '.php'; ?>
-		<?php include 'layout/footer.blade.php'; ?>
+		<?php include 'footer.php'; ?>
     </div>
 </div>
 </body>
-<?php include 'layout/modal.blade.php'; ?>
-<?php include 'layout/includes_bottom.blade.php'; ?>
+<?php include 'modal.php'; ?>
+<?php include 'includes_bottom.php'; ?>
